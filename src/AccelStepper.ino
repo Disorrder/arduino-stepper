95f7653e5608384ae69cb4f1da9830a932e92c2c
Disorder <disorrder@gmail.com>
2021-07-31 19:24:32 +0300

    try AccelStepper

--- src/AccelStepper.ino
+++ src/AccelStepper.ino
@@ -1,7 +1,8 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
-#define STEPS 200
+#define STEPS       200
+#define MICROSTEPS  16
 
 #define S1_EN   1
 #define S1_DIR  2
@@ -10,13 +11,19 @@
 #define S2_DIR  5
 #define S2_STEP 6
 
-AccelStepper stepper1(STEPS, S1_STEP, S1_DIR);
-// AccelStepper stepper2(STEPS, S2_STEP, S2_DIR);
+AccelStepper stepper1(AccelStepper::DRIVER, S1_STEP, S1_DIR);
 
 void setup() {
-
+    stepper1.setEnablePin(S1_EN);
+    stepper1.setMaxSpeed(STEPS * 1);
+    stepper1.setAcceleration(STEPS);
+    stepper1.moveTo(STEPS * 10);
 }
 
 void loop() {
-
+    if (stepper1.distanceToGo() == 0) {
+        // stepper1.disableOutputs();
+        stepper1.moveTo(-stepper1.currentPosition());
+    }
+    stepper1.run();
 }
